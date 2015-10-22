453fdce4876f3373e99cc5fa5dd44e44192c872d
Luis Bravo <luisbravo258@gmail.com>
2015-10-22 17:25:17 -0400

    asig of ruby table

---
+++ asig.rb
@@ -0,0 +1,20 @@
+line_width = 50
+
+
+chapterone = "Chapter 1: Gets Started"
+chaptertwo = "Chapter 2: All about cars"
+chapterthree = "Chapter 3: The Parts of the Motor"
+
+one = "Page 1"
+two = "Page 2"
+three = "Page 3"
+
+puts "Table of Content".center(56)
+
+puts "=" * 56
+
+puts chapterone.ljust(line_width) + one.rjust(0)
+
+puts chaptertwo.ljust(line_width) + two.rjust(0)
+
+puts chapterthree.ljust(line_width) + three.rjust(0)
