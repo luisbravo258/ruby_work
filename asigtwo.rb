0df4a4aaf0fbef48b57f983cda654c7921de04df
Luis Bravo <luisbravo258@gmail.com>
2015-10-28 22:17:45 -0400

    grandma one

---
+++ asigtwo.rb
@@ -0,0 +1,12 @@
+while
+  input = gets.chomp
+  puts "HUH SPEAK UP"
+
+if input.upcase == 'HELLO'
+  puts "NOT SINCE 1942!"
+end
+   if input.upcase == 'GOODBYE'
+    puts "chao amore"
+    break
+  end
+end
